a0cbc934cb6b81d8506745c26a11dab9d72ed440
Nochi <48821257+ainochi-kor@users.noreply.github.com>
2025-12-07 01:28:05 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251207/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    double a,b,c;
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a;
+    cout << '\n';
+    cout << b;
+    cout << '\n';
+    cout << c;
+    return 0;
+}
