45a94bbc69da292987c264aaf41c635e117e4abf
Nochi <48821257+ainochi-kor@users.noreply.github.com>
2025-12-07 19:11:39 +0900

    Title: B부터 A까지 감소, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251207/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    for (b; b >= a; b-- ){
+        cout << b << " ";
+    }
+
+
+    return 0;
+}
