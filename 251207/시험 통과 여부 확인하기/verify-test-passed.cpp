1444c048f87b9699871e5afecb9a55ac8fc8888c
Nochi <48821257+ainochi-kor@users.noreply.github.com>
2025-12-07 17:37:04 +0900

    Title: 시험 통과 여부 확인하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251207/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    cout << (n >= 80 ? "pass" : "x more score");
+    return 0;
+}
