91e0e87f47dc4c4ed3f0b7ef06c71c6ee34ebb2a
Nochi <48821257+ainochi-kor@users.noreply.github.com>
2025-12-07 16:31:07 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251207/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    cout << n;
+
+    if(n < 0) {
+        cout << "\nminus";
+    }
+    
+    return 0;
+}
